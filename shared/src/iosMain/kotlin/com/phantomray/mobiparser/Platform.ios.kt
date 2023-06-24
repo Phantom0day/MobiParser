2956103d852764abac3b7193c8d62e8d24a799cc
PhantomRay <PhantomNet001@gmail.com>
2023-06-24 10:03:33 +1000

    Init project

---
+++ shared/src/iosMain/kotlin/com/phantomray/mobiparser/Platform.ios.kt
@@ -0,0 +1,9 @@
+package com.phantomray.mobiparser
+
+import platform.UIKit.UIDevice
+
+class IOSPlatform: Platform {
+    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+}
+
+actual fun getPlatform(): Platform = IOSPlatform()
